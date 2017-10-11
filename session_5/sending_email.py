d1215d5bb3958597b3719c5e34b6293a6579dbb6
ashleighyslop <ahyslop2@sapient.com>
2017-10-11 20:27:38 +0100

    hid sensitive info

--- session_5/sending_email.py
+++ session_5/sending_email.py
@@ -18,9 +18,9 @@ def signup():
 
 def send_simple_message(email, first, second):
     return requests.post(
-        "https://api.mailgun.net/v3/sandboxa506192b06074cb1bec955c3c6b08c0f.mailgun.org/messages",
-        auth=("api", "key-b8f6655ee28db779a9e6ad1307a17b4c"),
-        data={"from": "Excited User <mailgun@sandboxa506192b06074cb1bec955c3c6b08c0f.mailgun.org>",
+        "API KEY",
+        auth=("api", "API KEY"),
+        data={"from": "Excited User <DOMAIN NAME>",
               "to": [email],
               "subject": "Hello",
               "text": "Hello " + first + " " + second + "!"})
